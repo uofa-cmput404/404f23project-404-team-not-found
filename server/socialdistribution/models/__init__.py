29a109f6e09dd8e160d25080f0069cc918fef146
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-15 22:05:20 -0600

    refactor: move into separate files

---
+++ server/socialdistribution/models/__init__.py
@@ -0,0 +1,10 @@
+"""
+Module for all models in the app
+"""
+
+# Django Software Foundation, "Organizing models in a package"
+# https://docs.djangoproject.com/en/4.2/topics/db/models/#organizing-models-in-a-package
+
+from .author import Author
+from .category import Category
+from .post import Post
--- server/socialdistribution/models/post.py
+++ server/socialdistribution/models/post.py
@@ -1,25 +1,5 @@
 from django.db import models
-from .constants import CONTENT_MAXLEN, STRING_MAXLEN, URL_MAXLEN
-
-
-# Create your models here.
-class Author(models.Model):
-    """
-    An author is a basic user in the social distribution app.
-    """
-    createdAt = models.DateTimeField(auto_now_add=True)
-    displayName = models.CharField(max_length=STRING_MAXLEN)
-    github = models.URLField(max_length=URL_MAXLEN, null=True)
-    host = models.URLField(max_length=URL_MAXLEN)   # home host of the author
-    profileImage = models.URLField(max_length=URL_MAXLEN)
-    url = models.URLField(max_length=URL_MAXLEN)     # url to the author's profile
-
-
-class Category(models.Model):
-    """
-    Categories for posts
-    """
-    category = models.CharField(max_length=STRING_MAXLEN)
+from socialdistribution.constants import CONTENT_MAXLEN, STRING_MAXLEN, URL_MAXLEN
 
 
 class Post(models.Model):
@@ -40,9 +20,9 @@ class Post(models.Model):
         PRIVATE = "PR", "PRIVATE"
         PUBLIC = "PB", "PUBLIC"
 
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
     # ManyToMany rel in Django: sankalpjonna.com/learn-django/the-right-way-to-use-a-manytomanyfield-in-django
-    categories = models.ManyToManyField(Category, blank=True)
+    categories = models.ManyToManyField("Category", blank=True)
 
     content = models.BinaryField(max_length=CONTENT_MAXLEN, null=True, blank=True)
     contentType = models.CharField(choices=ContentType.choices, max_length=STRING_MAXLEN, default=ContentType.PLAIN)
