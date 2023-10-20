562be449cce9f166d4e0b9d2080ecd53ad5033b2
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-20 17:10:47 -0600

    cleanup: add comments

--- server/socialdistribution/serializers.py
+++ server/socialdistribution/serializers.py
@@ -10,11 +10,12 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    id = SerializerMethodField('get_id_url')
+    # SerializerMethodField Reference: https://testdriven.io/blog/drf-serializers/
+    id = SerializerMethodField("get_id_url")
     author = AuthorSerializer(many=False, read_only=True)
     content = SerializerMethodField("get_content")
-    origin = SerializerMethodField('get_origin_url')
-    source = SerializerMethodField('get_source_url')
+    origin = SerializerMethodField("get_origin_url")
+    source = SerializerMethodField("get_source_url")
     type = SerializerMethodField("get_type")
 
     class Meta:
@@ -23,27 +24,30 @@ class PostSerializer(serializers.ModelSerializer):
                   "origin", "published", "updatedAt", "visibility", "unlisted")
 
     def get_id_url(self, obj):
-        """id of the post"""
-        uri = self.context['request'].build_absolute_uri('/')
-        return f'{uri}author/{obj.author.id}/posts/{obj.id}'
+        # id field needs to be a uri of the post
+        uri = self.context["request"].build_absolute_uri("/")
+        return f"{uri}author/{obj.author.id}/posts/{obj.id}"
 
     def get_content(self, obj):
+        # decode content as it's a binary field
         if obj.contentType == Post.ContentType.PLAIN and obj.content:
             return obj.content.decode("utf-8")
 
     def get_origin_url(self, obj):
+        # if source is given, pass in the origin, otherwise, build it using current request uri
         if obj.origin:
             return obj.origin
         else:
-            uri = self.context['request'].build_absolute_uri('/')
-            return f'{uri}author/{obj.author.id}/posts/{obj.id}'
+            uri = self.context["request"].build_absolute_uri("/")
+            return f"{uri}author/{obj.author.id}/posts/{obj.id}"
 
     def get_type(self, obj):
         return "post"
 
     def get_source_url(self, obj):
+        # if source is given, pass in the source, otherwise, build it using current request uri
         if obj.source:
             return obj.source
         else:
-            uri = self.context['request'].build_absolute_uri('/')
-            return f'{uri}author/{obj.author.id}/posts/{obj.id}'
+            uri = self.context["request"].build_absolute_uri("/")
+            return f"{uri}author/{obj.author.id}/posts/{obj.id}"
