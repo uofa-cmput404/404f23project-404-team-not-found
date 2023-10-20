5c89e70dbd1620dd73e4c687eb1016dab4f06d5a
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-20 17:13:18 -0600

    refactor: use get_object_or_404 directly on api calls

--- server/socialdistribution/views.py
+++ server/socialdistribution/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -21,15 +21,13 @@ class PostsView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, author_id):
-        try:
-            author = Author.objects.get(id=author_id)
-        except Author.DoesNotExist:
-            return Response({"error": "Author not found."}, status=status.HTTP_404_NOT_FOUND)
-
-        post_object = create_post(author, request.data)
+        # get_object_or_404 is a django shortcut
+        # Reference: https://docs.djangoproject.com/en/4.2/topics/http/shortcuts/#get-object-or-404
+        author_obj = get_object_or_404(Author, id=author_id)
+        post_object = create_post(author_obj, request.data)
         serializer = PostSerializer(instance=post_object, data=request.data, context={"request": request})
         if serializer.is_valid():
-            serializer.save(author=author)
+            serializer.save(author=author_obj)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
