59aedfab5dbea0238b7a11a55afa69812383b109
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-20 18:50:19 -0600

    update PostView with get, post, put
    - added DELETE method, but unused

--- server/socialdistribution/views.py
+++ server/socialdistribution/views.py
@@ -47,5 +47,49 @@ class PostsView(APIView):
 
 
 class PostView(APIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    http_method_names = ["delete", "get", "post", "put"]
+
+    def delete(self, request, author_id, post_id):
+        """
+        remove the post whose id is POST_ID
+        TODO: not doing as it's another user story
+        """
+        pass
+
+    def get(self, request, author_id, post_id):
+        """
+        get the public post whose id is POST_ID
+        """
+        post_object = get_object_or_404(Post, id=post_id, author__id=author_id)
+        serializer = PostSerializer(post_object, context={"request": request})
+
+        return Response(serializer.data)
+
+    def post(self, request, author_id, post_id):
+        """
+        update the post whose id is POST_ID (must be authenticated)
+        TODO: update this when authentication is implemented
+        """
+        post_object = get_object_or_404(Post, id=post_id, author__id=author_id)
+        serializer = PostSerializer(instance=post_object, data=request.data, context={"request": request})
+
+        if serializer.is_valid():
+            # save update and set updatedAt to current time
+            serializer.save(updatedAt=timezone.now())
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, author_id, post_id):
+        """
+        create a post where its id is POST_ID
+        """
+        author_obj = get_object_or_404(Author, id=author_id)
+        post_object = create_post(author_obj, request.data, post_id)
+        serializer = PostSerializer(instance=post_object, data=request.data, context={"request": request})
+
+        if serializer.is_valid():
+            serializer.save(author=author_obj)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
