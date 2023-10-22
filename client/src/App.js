582aa0906ef490db4afdb8791f6282d474efb570
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2023-10-21 22:18:04 -0600

    Added route for homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-import HomePage from './components/HomePage';
-
+import "./App.css";
+import HomePage from "./components/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 const App = () => {
   return (
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home-page" element={<HomePage />} />{" "}
+        {/* 👈 Renders at /app/ */}
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
