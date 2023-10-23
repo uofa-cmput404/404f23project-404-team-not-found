e92b2593f7a547b23b0ac4879af4b2f2f19ddc85
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2023-10-22 18:57:23 -0600

    Finalized sign up

--- client/src/components/authentication/SignUp.tsx
+++ client/src/components/authentication/SignUp.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -8,9 +8,46 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 
+import axios from "axios";
+
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
   const handleSubmit = (e: FormEvent) => {
-    return;
+    e.preventDefault();
+
+    if (formData.password !== formData.repeatPassword) {
+      toast.error("Repeat password does not match!");
+      return;
+    }
+
+    const requestUrl = "http://127.0.0.1:8000/socialdistribution/signup/";
+
+    const form = new FormData();
+    form.append("username", formData.username);
+    form.append("email", formData.email);
+    form.append("password", formData.password);
+
+    axios
+      .post(requestUrl, form, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((response: any) => {
+        toast.success("You have succesfully signed up");
+        navigate("/login");
+      })
+      .catch((error) => {
+        toast.error(error.response.data.message);
+      });
   };
 
   return (
@@ -34,10 +71,24 @@ const SignUp = () => {
             margin="normal"
             required
             fullWidth
+            id="username"
+            label="Enter your username"
+            name="username"
+            autoFocus
+            onChange={(e) => {
+              setFormData({ ...formData, username: e.target.value });
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
             id="email"
             label="Enter your email"
             name="email"
-            autoFocus
+            onChange={(e) => {
+              setFormData({ ...formData, email: e.target.value });
+            }}
           />
           <TextField
             margin="normal"
@@ -47,15 +98,21 @@ const SignUp = () => {
             label="Create password"
             type="password"
             id="password"
+            onChange={(e) => {
+              setFormData({ ...formData, password: e.target.value });
+            }}
           />
           <TextField
             margin="normal"
             required
             fullWidth
-            name="repeat-password"
+            name="repeatPassword"
             label="Confirm password"
             type="password"
-            id="password"
+            id="repeatPassword"
+            onChange={(e) => {
+              setFormData({ ...formData, repeatPassword: e.target.value });
+            }}
           />
           <Button
             type="submit"
