c4c75db1f8ea06d74a16bd4b7fa9944ff7d19f70
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2023-10-22 18:02:43 -0600

    Added toastify for toast message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,14 +7,29 @@ import Login from "./components/authentication/Login";
 import SignUp from "./components/authentication/SignUp";
 
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/home-page" element={<HomePage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
-    </Routes>
+    <>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Routes>
+        <Route path="/home-page" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </>
   );
 };
 
