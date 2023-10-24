a6e7015c3a081a571c9853362c0e42f402b6faba
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-24 07:44:14 -0600

    tests: tests.. for follower, followers, partial inbox

--- server/socialdistribution/tests/utils/tests_utils.py
+++ server/socialdistribution/tests/utils/tests_utils.py
@@ -1,4 +1,7 @@
 import json
+import uuid
+
+from django.contrib.contenttypes.models import ContentType
 
 from socialdistribution.models import *
 
@@ -15,15 +18,59 @@ def create_author():
     return author_obj
 
 
+def create_author_dict(author_id):
+    author_dict = {
+        "type": "author",
+        "id": f"http://127.0.0.1:5454/authors/{author_id}",
+        "url": f"http://127.0.0.1:5454/authors/{author_id}",
+        "host": "http://127.0.0.1:5454/",
+        "displayName": "test_not_found",
+        "github": "http://github.com/gjohnson",
+        "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+    }
+
+    return author_dict
+
+
+def create_follow(author, actor):
+    follow_object = Follow.objects.create(
+        id=uuid.uuid4(),
+        object=author,
+        actor=actor
+    )
+
+    return follow_object
+
+
+def create_follow_inbox_item(follow_object, inbox_object):
+    content_type = ContentType.objects.get_for_model(follow_object)
+    inbox_item_object = InboxItem.objects.create(content_type=content_type,
+                                                 object_id=follow_object.id,
+                                                 content_object=follow_object)
+    inbox_object.items.add(inbox_item_object)
+
+    print(inbox_item_object)
+    return inbox_item_object
+
+
+def create_follower(author, follower_author):
+    follower_obj = Follower.objects.create(
+        author=author,
+        follower_author=follower_author
+    )
+
+    return follower_obj
+
+
 def create_plain_text_post(author):
     post_obj = Post.objects.create(
-            author=author,
-            title="Test Post",
-            description="This is a test post.",
-            contentType=Post.ContentType.PLAIN,
-            content="This is test content.".encode("utf-8"),
-            visibility=Post.Visibility.PUBLIC
-        )
+        author=author,
+        title="Test Post",
+        description="This is a test post.",
+        contentType=Post.ContentType.PLAIN,
+        content="This is test content.".encode("utf-8"),
+        visibility=Post.Visibility.PUBLIC
+    )
 
     return post_obj
 
---
+++ server/socialdistribution/tests/views/test_inbox_view.py
@@ -0,0 +1,89 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
+from socialdistribution.models import InboxItem, Inbox
+from socialdistribution.tests.utils import (
+    create_author,
+    create_author_dict,
+    create_follow,
+    create_follow_inbox_item,
+    create_follower,
+    deserialize_response
+)
+
+
+class TestInboxView(TestCase):
+    def setUp(self):
+        """
+        Setting up the tests. This will run before any of the test methods.
+        """
+        self.client = APIClient()
+        self.author = create_author()
+        self.follower = create_author()
+        self.url = reverse("inbox", args=[self.author.id])
+
+    def test_delete_to_clear_inbox(self):
+        inbox_object = Inbox.objects.get(author_id=self.author.id)
+        author_json = create_author_dict(author_id=self.follower.id)
+
+        follow_object = create_follow(self.author, author_json)
+        create_follow_inbox_item(follow_object, inbox_object)
+
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(InboxItem.objects.filter(inbox=inbox_object).exists())
+
+    def test_delete_to_clear_empty_inbox(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_get_with_empty_inbox(self):
+        response = self.client.get(self.url)
+        json_obj = deserialize_response(response)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json_obj["type"], "inbox")
+        self.assertEqual(len(json_obj["items"]), 0)
+
+    def test_get_with_follow_item_in_inbox(self):
+        inbox_object = Inbox.objects.get(author_id=self.author.id)
+        author_json = create_author_dict(author_id=self.follower.id)
+
+        follow_object = create_follow(self.author, author_json)
+        create_follow_inbox_item(follow_object, inbox_object)
+
+        response = self.client.get(self.url)
+        json_obj = deserialize_response(response)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(json_obj["type"], "inbox")
+        self.assertEqual(len(json_obj["items"]), 1)
+        self.assertEqual(json_obj["items"][0]["type"], "Follow")
+
+    def test_post_follow_to_inbox(self):
+        data = {
+            "type": "Follow",
+            "summary": "Zz wanna follow u",
+            "actor": create_author_dict(author_id=self.follower.id),
+            "object": create_author_dict(author_id=self.author.id)
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+        json_obj = deserialize_response(response)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(json_obj["type"], "Follow")
+        self.assertEqual(json_obj["actor"]["type"], "author")
+
+    def test_post_invalid_without_type(self):
+        data = {
+            "summary": "Zz wanna follow u",
+            "actor": create_author_dict(author_id=self.follower.id),
+            "object": create_author_dict(author_id=self.author.id)
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
