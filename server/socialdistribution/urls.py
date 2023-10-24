fe048ed55d688a649517de424066a33fdc57ba6d
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-23 21:54:14 -0600

    comments: add comments in urls.py for organization

--- server/socialdistribution/urls.py
+++ server/socialdistribution/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # API endpoints based on the outlined spec for the Distributed Social Networking app:
+    # 
     path("author/<uuid:author_id>/", AuthorView.as_view(), name="author"),
     path("author/<uuid:author_id>/posts/", PostsView.as_view(), name="posts"),
     path("author/<uuid:author_id>/posts/<uuid:post_id>/", PostView.as_view(), name="single_post"),
+
+    # login and signup endpoints
     path("login/", LoginView.as_view(), name="login"),
     path("signup/", SignUpView.as_view(), name="signup")
 ]
