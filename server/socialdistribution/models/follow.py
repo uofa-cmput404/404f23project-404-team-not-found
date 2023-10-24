845699fe69e169a904f9a0fc26b2fb13ad419576
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-23 22:16:17 -0600

    cleanup: alphabetize fields in Follow model

--- server/socialdistribution/models/follow.py
+++ server/socialdistribution/models/follow.py
@@ -6,5 +6,5 @@ class Follow(models.Model):
     The Follow handles the follow/friend requests of a given author (requestor)
     to another author (recipient).
     """
-    requestor = models.JSONField()
     recipient = models.ForeignKey("Author", on_delete=models.CASCADE)
+    requestor = models.JSONField()
