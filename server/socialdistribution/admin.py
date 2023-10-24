be716b3cb0ea0bf2161eebc138b3708748221bca
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-24 15:35:15 -0600

    register models to admin

--- server/socialdistribution/admin.py
+++ server/socialdistribution/admin.py
@@ -4,4 +4,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Author)
+admin.site.register(Follow)
+admin.site.register(Follower)
+admin.site.register(Inbox)
 admin.site.register(Post)
