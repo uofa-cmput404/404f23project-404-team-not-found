3b76594827066787acd4e0c690210d518f7c631a
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-10-26 13:58:54 -0600

    feat : comments uri builder

--- server/socialdistribution/utils/serializers_utils.py
+++ server/socialdistribution/utils/serializers_utils.py
@@ -9,3 +9,8 @@ def build_default_author_uri(obj, request):
 def build_default_post_uri(obj, request):
     uri = request.build_absolute_uri("/")
     return f"{uri}{SERVICE}author/{obj.author.id}/posts/{obj.id}"
+
+
+def build_default_comment_uri(obj, request):
+    uri = request.build_absolute_uri("/")
+    return f"{uri}{SERVICE}author/{obj.author.id}/posts/{obj.post.id}/comments/{obj.id}"
