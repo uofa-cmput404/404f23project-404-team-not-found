fa63d88341b0bbd7f7cdc1b499e87e8e200cd2f4
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-10-26 18:37:40 -0600

    feat: comment serializer url

--- server/socialdistribution/urls.py
+++ server/socialdistribution/urls.py
@@ -7,12 +7,28 @@ urlpatterns = [
     # https://github.com/uofa-cmput404/project-socialdistribution/blob/master/project.org
     path(f"{SERVICE}author/<uuid:author_id>/", AuthorView.as_view(), name="author"),
     path(f"{SERVICE}author/<uuid:author_id>/posts/", PostsView.as_view(), name="posts"),
-    path(f"{SERVICE}author/<uuid:author_id>/posts/<uuid:post_id>/", PostView.as_view(), name="single_post"),
-    path(f"{SERVICE}author/<uuid:author_id>/followers/", FollowersView.as_view(), name="followers"),
-    path(f"{SERVICE}author/<uuid:author_id>/followers/<uuid:follower_id>/", FollowerView.as_view(), name="follower"),
+    path(
+        f"{SERVICE}author/<uuid:author_id>/posts/<uuid:post_id>/",
+        PostView.as_view(),
+        name="single_post",
+    ),
+    path(
+        f"{SERVICE}author/<uuid:author_id>/followers/",
+        FollowersView.as_view(),
+        name="followers",
+    ),
+    path(
+        f"{SERVICE}author/<uuid:author_id>/followers/<uuid:follower_id>/",
+        FollowerView.as_view(),
+        name="follower",
+    ),
     path(f"{SERVICE}author/<uuid:author_id>/inbox/", InboxView.as_view(), name="inbox"),
-
+    path(
+        f"{SERVICE}author/<uuid:author_id>/posts/<uuid:post_id>/comments/",
+        CommentView.as_view(),
+        name="comments",
+    ),
     # login and signup endpoints
     path(f"{SERVICE}login/", LoginView.as_view(), name="login"),
-    path(f"{SERVICE}signup/", SignUpView.as_view(), name="signup")
+    path(f"{SERVICE}signup/", SignUpView.as_view(), name="signup"),
 ]
