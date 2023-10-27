63f403402248ef264b4b5a8ad8a86faec77d5602
rmgutierrez <raphael_0810@outlook.com>
2023-10-26 19:37:17 -0600

    Import values from enum for readability

--- client/src/components/post/VisibilityMenu.tsx
+++ client/src/components/post/VisibilityMenu.tsx
@@ -5,27 +5,31 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 import PublicIcon from '@mui/icons-material/Public';
 import LockIcon from '@mui/icons-material/Lock';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import GroupIcon from '@mui/icons-material/Group';
 
+import { ShareType } from "../../enums/enums";
+
 const VisibilityMenu = (props:any) => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const [text, setText] = useState("Public")
   const [icon, setIcon] = useState(<PublicIcon/>)
   const open = Boolean(anchorEl);
+
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   return (
-    <div>
+    <Grid container direction="column">
       <Button
         id="basic-button"
         aria-controls={open ? 'basic-menu' : undefined}
@@ -33,8 +37,8 @@ const VisibilityMenu = (props:any) => {
         aria-expanded={open ? 'true' : undefined}
         onClick={handleClick}
         startIcon={icon}
-        sx={{marginLeft:1}}
-        size="small"
+        sx={{marginLeft:1, marginRight: "auto"}}
+        size="medium"
       >
         <Typography textTransform="none">{text}</Typography>
       </Button>
@@ -49,7 +53,7 @@ const VisibilityMenu = (props:any) => {
       >
         <MenuItem 
         onClick={() => {
-          props.setVisibility("PUBLIC");
+          props.setVisibility(ShareType.PUBLIC);
           props.setUnlisted(false);
           setText("Public");
           setIcon(<PublicIcon/>);
@@ -63,7 +67,7 @@ const VisibilityMenu = (props:any) => {
         </MenuItem>
         <MenuItem 
         onClick={() => {
-          props.setVisibility("PRIVATE");
+          props.setVisibility(ShareType.PRIVATE);
           props.setUnlisted(false)
           setText("Private");
           setIcon(<LockIcon/>);
@@ -77,7 +81,7 @@ const VisibilityMenu = (props:any) => {
         </MenuItem>
         <MenuItem 
         onClick={() => {
-          props.setVisibility("FRIENDS");
+          props.setVisibility(ShareType.FRIENDS);
           props.setUnlisted(false)
           setText("Friends");
           setIcon(<GroupIcon/>)
@@ -91,7 +95,7 @@ const VisibilityMenu = (props:any) => {
         </MenuItem>
         <MenuItem 
         onClick={() => {
-          props.setVisibility("PUBLIC");
+          props.setVisibility(ShareType.PUBLIC);
           props.setUnlisted(true)
           setText("Unlisted");
           setIcon(<VisibilityOffIcon/>)
@@ -104,7 +108,7 @@ const VisibilityMenu = (props:any) => {
           <ListItemText>Unlisted</ListItemText>
         </MenuItem>
       </Menu>
-    </div>
+    </Grid>
   );
 }
 
