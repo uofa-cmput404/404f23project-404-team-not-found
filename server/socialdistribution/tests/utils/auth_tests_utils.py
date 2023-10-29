9149546d7be97c379898408e188fff02202edd10
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2023-10-28 23:18:45 -0600

    Added tests for authentication

---
+++ server/socialdistribution/tests/utils/auth_tests_utils.py
@@ -0,0 +1,45 @@
+import json
+import uuid
+
+from django.contrib.contenttypes.models import ContentType
+
+from django.contrib.auth.models import User
+from socialdistribution.models import *
+
+
+def create_auth_user():
+    user_data = {
+        "username": "test_user",
+        "email": "test_user@gmail.com",
+        "password": "123456",
+    }
+
+    user_obj = User.objects.create_user(username=user_data["username"], 
+                                        email=user_data["email"], 
+                                        password=user_data["password"])
+    
+    return user_obj
+
+def create_auth_author(user_obj):
+    # TODO: update when author/user implementation is done for better testing
+
+    
+    author_data = {"displayName": "test_author", 
+                    "github": "https://placeholder.com", 
+                    "host": "https://placeholder.com",
+                    "profileImage": "https://placeholder.com",
+                    "url": "https://placeholder.com"}
+
+
+
+    auth_obj = Author.objects.create(displayName=author_data["displayName"], 
+                                    github=author_data["github"],
+                                    host=author_data["host"],
+                                    profileImage=author_data["profileImage"],
+                                    url=author_data["url"],
+                                    user=user_obj)
+
+    return auth_obj
+
+def deserialize_response(response):
+    return json.loads(response.content.decode('utf-8').replace("'", "\""))
