bd7251e1e6b9487312dfee6b3f8f5a3890e20aeb
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-30 12:51:53 -0600

    update test deployment

--- server/server/settings.py
+++ server/server/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '../client/build/static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, '../client/build')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
