db32cf46d212dbb0bb424c13f1dee2687bd858db
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-10-30 00:09:46 -0600

    update spacing based from main
    Co-Authored-By: Boredalien248 <kaustubh248@gmail.com>

--- client/src/components/Profilepage/ProfilePage.tsx
+++ client/src/components/Profilepage/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Typography, CssBaseline, Container, Button, Theme, Modal, Box, TextField } from "@mui/material"
+import { Typography, CssBaseline, Container, Button, Theme, Modal, Box, TextField } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { Post } from "../../interfaces/interfaces";
 import "./styles.css";
@@ -10,19 +10,18 @@ import { getAuthorId } from "../../utils/localStorageUtils";
 import HeadBar from "../template/AppBar";
 import { Author } from "../../interfaces/interfaces";
 import EditIcon from '@mui/icons-material/Edit';
-import { ImageLink } from "../../enums/enums";
 
 const APP_URI = process.env.REACT_APP_URI;
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
     backgroundColor: "#FAF8F1",
-    paddingTop: "2rem",
+    padding: "2rem",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center", 
     alignItems: "center",
-    position: 'relative'
+    position: "relative",
   },
   picture: {
     maxWidth: 200,
@@ -131,129 +130,129 @@ const ProfilePage = () => {
   }, []);
 
     const handleOpen = () => {
-        setOpen(true);
+      setOpen(true);
     };
     
     const handleClose = () => {
-        setOpen(false);
+      setOpen(false);
     };
     
     const handleSave = async () => {
-        const AUTHOR_ID = getAuthorId();
-        const url = `${APP_URI}author/${AUTHOR_ID}/`;
+      const AUTHOR_ID = getAuthorId();
+      const url = `${APP_URI}author/${AUTHOR_ID}/`;
 
-        const formData = new FormData();
+      const formData = new FormData();
 
-        if (userinfo.displayName) {
-            formData.append("displayName", userinfo.displayName);
+      if (userinfo.displayName) {
+        formData.append("displayName", userinfo.displayName);
+      } else {
+        formData.append("displayName", username ?? "");
+      }
+
+      if (userinfo.github) {
+        formData.append("github", userinfo.github);
+      } else {
+        formData.append("github", "");
+      }
+
+      if (userinfo.profileImage) {
+        formData.append("profileImage", userinfo.profileImage);
+      } else {
+        formData.append("profileImage", defaultSrc);
+      }
+
+      try {
+        const response = await axios.post(url, formData, {
+          headers: {
+              'Content-Type': 'multipart/form-data',
+          },
+        });
+        if (response.status === 200) {
+          toast.success("Profile updated successfully");
+          handleClose();
+          fetchAuthors();
         } else {
-            formData.append("displayName", username ?? "");
+          toast.error("Failed to update profile");
         }
-    
-        if (userinfo.github) {
-            formData.append("github", userinfo.github);
-        } else {
-            formData.append("github", "");
-        }
-
-        if (userinfo.profileImage) {
-            formData.append("profileImage", userinfo.profileImage);
-        } else {
-            formData.append("profileImage", defaultSrc);
-        }
-
-        try {
-            const response = await axios.post(url, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            if (response.status === 200) {
-                toast.success("Profile updated successfully");
-                handleClose(); 
-                fetchAuthors();
-            } else {
-                toast.error("Failed to update profile");
-            }
-        } catch (error) {
-            toast.error("Failed to update profile");
-        }
+      } catch (error) {
+        toast.error("Failed to update profile");
+      }
     };
 
-    return (
+  return (
     <>
-        <CssBaseline />
-        <HeadBar />
-        <main>
-            <div className={classes.container}>
-                <div className={classes.content}>
-                    <div>
-                    <img src={profilePic || defaultSrc} alt="profile-pic" className={classes.picture} />
-                        <Typography variant="h2" align="center" color="textPrimary" style={{ fontFamily: 'Bree Serif, serif' }}>
-                            {username}
-                        </Typography>
-                        <a href={github ?? ""} target="_blank" rel="noopener noreferrer">
-                            <Typography align="center" variant="body2" color="primary">
-                                {github}
-                            </Typography>
-                        </a>
-                    </div>
-                </div>
-                <div>
-                    <Button variant="contained" style={{top: "10px"}} onClick={handleOpen}>
-                        <EditIcon />
-                        <Typography>. EDIT INFO</Typography>
-                    </Button>
-                    <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="modal-modal-title"
-                        aria-describedby="modal-modal-description"
-                        className={classes.modal}
-                    >
-                        <Box className={classes.paper}>
-                            <Typography id="modal-modal-title" variant="h6" component="h2">
-                                EDIT PROFILE
-                            </Typography>
-                            <img src={userinfo.profileImage || defaultSrc} alt="profile-pic" className={classes.picture} />
-                            <TextField
-                                id="outlined-basic"
-                                label="Display Name"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userinfo.displayName}    
-                                onChange={(e) => setUserinfo({...userinfo, displayName: e.target.value})}
-                            />
-                            <TextField
-                                id="outlined-basic"
-                                label="Github Link"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userinfo.github}
-                                onChange={(e) => setUserinfo({...userinfo, github: e.target.value})}
-                            />
-                            <TextField
-                                id="outlined-basic"
-                                label="Image Link"
-                                variant="outlined"
-                                fullWidth
-                                margin="normal"
-                                value={userinfo.profileImage}
-                                onChange={(e) => setUserinfo({...userinfo, profileImage: e.target.value})}
-                            />
-                            <Button variant="contained" color="primary" className={classes.save_button} onClick={handleSave}>
-                                Save
-                            </Button>
-                        </Box>
-                    </Modal>
-                </div>
+      <CssBaseline />
+      <HeadBar />
+      <main>
+        <div className={classes.container}>
+          <div className={classes.content}>
+            <div>
+            <img src={profilePic || defaultSrc} alt="profile-pic" className={classes.picture} />
+              <Typography variant="h2" align="center" color="textPrimary" style={{ fontFamily: 'Bree Serif, serif' }}>
+                {username}
+              </Typography>
+              <a href={github ?? ""} target="_blank" rel="noopener noreferrer">
+                <Typography align="center" variant="body2" color="primary">
+                  {github}
+                </Typography>
+              </a>
             </div>
-            <Container className={classes.cardGrid} maxWidth="md">
-            <PostsList posts={posts} deletePost={deletePost} onPostEdited={fetchPosts} />
-            </Container>
-        </main>
+          </div>
+            <div>
+              <Button variant="contained" style={{top: "10px"}} onClick={handleOpen}>
+                <EditIcon />
+                <Typography>. EDIT INFO</Typography>
+              </Button>
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                  className={classes.modal}
+                >
+                  <Box className={classes.paper}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                      EDIT PROFILE
+                    </Typography>
+                    <img src={userinfo.profileImage || defaultSrc} alt="profile-pic" className={classes.picture} />
+                    <TextField
+                      id="outlined-basic"
+                      label="Display Name"
+                      variant="outlined"
+                      fullWidth
+                      margin="normal"
+                      value={userinfo.displayName}
+                      onChange={(e) => setUserinfo({...userinfo, displayName: e.target.value})}
+                    />
+                      <TextField
+                        id="outlined-basic"
+                        label="Github Link"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        value={userinfo.github}
+                        onChange={(e) => setUserinfo({...userinfo, github: e.target.value})}
+                      />
+                      <TextField
+                        id="outlined-basic"
+                        label="Image Link"
+                        variant="outlined"
+                        fullWidth
+                        margin="normal"
+                        value={userinfo.profileImage}
+                        onChange={(e) => setUserinfo({...userinfo, profileImage: e.target.value})}
+                      />
+                      <Button variant="contained" color="primary" className={classes.save_button} onClick={handleSave}>
+                        Save
+                      </Button>
+                  </Box>
+                </Modal>
+            </div>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <PostsList posts={posts} deletePost={deletePost} onPostEdited={fetchPosts} />
+        </Container>
+      </main>
     </>
   );
 };
