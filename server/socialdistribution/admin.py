99425ee62d0e2ff31d1512543e88948af3ac34d1
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-11-08 14:45:19 -0700

    feat: registered comment model
    migrations !

--- server/socialdistribution/admin.py
+++ server/socialdistribution/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Author, Follow, Follower, Inbox, Post, Category, Comment
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(Follower)
 admin.site.register(Inbox)
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Comment)
