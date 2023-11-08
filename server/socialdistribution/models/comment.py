4490c3d5411f692403304a10d1846a2d9c3d844e
Mehsheed Ahmed Syed Abdul <mehsheed59@gmail.com>
2023-11-08 14:43:07 -0700

    feat : Added comment model

---
+++ server/socialdistribution/models/comment.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db import models
+from socialdistribution.utils.constants import CONTENT_MAXLEN, STRING_MAXLEN, URL_MAXLEN
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    author = models.ForeignKey("Author", on_delete=models.CASCADE)
+    comment = models.TextField(max_length=STRING_MAXLEN)
+    published = models.DateTimeField(auto_now_add=True)
+    contentType = models.TextField(default="text/plain", editable=False)
