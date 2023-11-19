d340335c9808d705b85ea852a69137b9916ef5e4
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-11-19 08:23:50 -0700

    refactor: create an AuthorsList component for reuse

--- client/src/components/follow/DiscoverContent.tsx
+++ client/src/components/follow/DiscoverContent.tsx
@@ -1,10 +1,11 @@
 import React, { useCallback, useEffect, useState } from "react";
 import axios from 'axios';
-import { Avatar, Button, Card, CardHeader, Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Author } from "../../interfaces/interfaces";
 import { getAuthorId, getUserData } from "../../utils/localStorageUtils";
 import { getAuthorIdFromResponse } from "../../utils/responseUtils";
 import { useNavigate } from "react-router-dom";
+import AuthorsList from "../author/AuthorsList";
 
 const APP_URI = process.env.REACT_APP_URI;
 
@@ -60,50 +61,9 @@ const DiscoverContent = () => {
       </Grid>
       <Grid container>
         {authors.length > 0 ?
-          (authors.map((author) => (
-          <Grid
-            container
-            key={author.id} 
-            alignItems="center"
-            sx={{
-              borderBottom: "1px solid #dbd9d9"
-            }}
-            >
-            <Grid item xs={6}>
-              <Card
-                style={{
-                  margin: "auto",
-                  width: "100%",
-                  border:0
-                }}
-                variant="outlined"
-              >
-                <CardHeader
-                  avatar={<Avatar src={author.profileImage} alt={author.displayName}/>}
-                  title={author.displayName}
-                />
-              </Card>
-            </Grid>
-            <Grid container item xs={6} justifyContent="flex-end">
-              <Button
-                variant="contained"
-                size="small"
-                color="primary"
-                sx={{
-                  borderRadius: 20,
-                  marginRight: 2,
-                  paddingLeft: 2,
-                  paddingRight: 2
-                }}
-                onClick={() => handleViewProfileClick(author)}
-              >
-                <Typography textTransform={"none"} variant="subtitle1">
-                  View Profile
-                </Typography>
-              </Button>
-            </Grid>
-          </Grid>
-        )))
+          (
+            <AuthorsList authors={authors} />
+          )
           : (
             <Typography
               variant="h6"
