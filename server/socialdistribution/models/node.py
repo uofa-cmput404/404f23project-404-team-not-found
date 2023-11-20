45a11c9f30726e45bae598357d23bb86def9aed1
Nhat Minh Luu <54903938+nluu175@users.noreply.github.com>
2023-11-19 19:00:04 -0700

    Updated Node primary key

--- server/socialdistribution/models/node.py
+++ server/socialdistribution/models/node.py
@@ -1,20 +1,15 @@
-import uuid
-
 from django.db import models
-from socialdistribution.utils.constants import STRING_MAXLEN, URL_MAXLEN
+from socialdistribution.utils.constants import STRING_MAXLEN
 from django.contrib.auth.models import User
 
 
 class Node(models.Model):
     """
     A node is an object that represents a team.
-    This is used to manage the permissions.
+    This is used to manage the permissions (is_allowed)
     """
 
-    # - Node model: name, host, username, password, is_allow(default=true). name primary key
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=STRING_MAXLEN)
+    name = models.CharField(primary_key=True, max_length=STRING_MAXLEN)
     host = models.CharField(max_length=STRING_MAXLEN)
     username = models.CharField(max_length=STRING_MAXLEN)
     password = models.CharField(max_length=STRING_MAXLEN)
