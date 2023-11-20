4daca04309515d108677f7d247ae87c5be2d6546
rmgutierrez <raphael_0810@outlook.com>
2023-11-20 02:58:51 -0700

    Click post from PostList to navigate to PostPage

--- client/src/components/post/PostsList.tsx
+++ client/src/components/post/PostsList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Like, Post } from "../../interfaces/interfaces";
-import { Avatar, Card, CardContent, CardHeader, Typography, CardMedia, Link , Grid, Button, IconButton } from "@mui/material";
+import { Avatar, Card, CardContent, CardHeader, Typography, CardMedia, Link, 
+  Grid, Button, IconButton, CardActionArea, ButtonBase } from "@mui/material";
 import { formatDateTime } from "../../utils/dateUtils";
 import { getAuthorId } from "../../utils/localStorageUtils";
 import { renderVisibility }from '../../utils/postUtils';
@@ -12,6 +13,8 @@ import MakeCommentModal from "../post/MakeCommentModal";
 import ShareIcon from '@mui/icons-material/Share';
 import Tooltip from '@mui/material/Tooltip';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import MoreMenu from './edit/MoreMenu';
 import styled from '@emotion/styled';
 import PostLikes from "./like/PostLikes";
@@ -34,11 +37,23 @@ const PostsList = ({
   const [postToComment, setPostToComment] = useState<Post>();
 
   const handleShare = () => { };
+  const navigate = useNavigate();
 
   const openMakeCommentModal = (post: Post) => {
     setPostToComment(post);
     setIsMakeCommentModalOpen(true);
   };
+
+  const handlePostClick = (authorId:string, postId:string, postArg: Post) => {
+    navigate(
+      `/${authorId}/posts/${postId}`,
+      {
+        state: {
+          post: postArg
+        }
+      }
+      )
+  }
   
   return (
     <Grid>
@@ -53,39 +68,87 @@ const PostsList = ({
             borderTop: 0
           }}
             variant='outlined'>
-          <CardHeader
-            avatar={<Avatar src={post.author.profileImage} alt={post.author.displayName} />}
-            action={
-              (getAuthorIdFromResponse(post.author.id) === getAuthorId() && post.visibility === 'PUBLIC') && (
-                <MoreMenu
-                  post={post}
-                  deletePost={deletePost}
-                  onPostEdited={onPostEdited}
-                />
+          <CardActionArea
+            disableRipple
+            onClick={event => {
+              handlePostClick(
+                getAuthorIdFromResponse(post.author.id),
+                getAuthorIdFromResponse(post.id),
+                post
+                );
+            }} 
+          >
+            <CardHeader
+              avatar={<Avatar src={post.author.profileImage} alt={post.author.displayName} />}
+              action={
+                (getAuthorIdFromResponse(post.author.id) === getAuthorId() && post.visibility === 'PUBLIC') && (
+                  <ButtonBase
+                    onMouseDown={event => event.stopPropagation()}
+                    sx={{borderRadius: 100}}
+                    disableRipple
+                    onClick={event => {
+                      event.stopPropagation();
+                      event.preventDefault();
+                    }}    
+                  >
+                    <MoreMenu
+                      post={post}
+                      deletePost={deletePost}
+                      onPostEdited={onPostEdited}
+                    />
+                  </ButtonBase>
+              )}
+              title={post.author.displayName}
+              subheader={post.updatedAt === null ? `${formatDateTime(post.published)} • ${renderVisibility(post)}` :
+              `${formatDateTime(post.published)} • ${renderVisibility(post)} • Edited`
+              }
+              sx = {{margin:0}}
+            />
+            <CardContent
+              sx={{
+                paddingTop: 0,
+                paddingLeft: 9,
+                paddingBottom: 0,
+                }}>
+              <Typography variant="h6">{post.title}</Typography>
+              <Typography variant="body1" marginBottom={1}>{post.description}</Typography>
+              {post.contentType === "text/plain" && post.content?.slice(0, 4) === "http" ? (
+              <div style={{paddingBottom: 0}}>
+                <Link href={post.content} target="_blank" noWrap>
+                  <Typography noWrap sx={{marginTop:1, marginBottom:0.5}}>
+                    {post.content}
+                  </Typography>
+                </Link>
+                <CardContentNoPadding sx={{ padding: 0, paddingBottom:0 }}>
+                  <div style={{ paddingBottom: 0 }}>
+                    <CardMedia
+                      component="img"
+                      style={{
+                        maxWidth: "100%",
+                        width: "auto",
+                        borderRadius: 12,
+                      }}
+                      image={post.content}
+                    />
+                  </div>
+                </CardContentNoPadding>
+              </div>
+            ):(
+              post.contentType === "text/plain" && (
+                <Typography variant="body1">{post.content}</Typography>)
             )}
-            title={post.author.displayName}
-            subheader={post.updatedAt === null ? `${formatDateTime(post.published)} • ${renderVisibility(post)}` :
-            `${formatDateTime(post.published)} • ${renderVisibility(post)} • Edited`
-            }
-            sx = {{margin:0}}
-          />
-          <CardContent
-            sx={{
-              paddingTop: 0,
-              paddingLeft: 9,
-              paddingBottom: 0,
-              }}>
-            <Typography variant="h6">{post.title}</Typography>
-            <Typography variant="body1" marginBottom={1}>{post.description}</Typography>
-            {post.contentType === "text/plain" && post.content?.slice(0, 4) === "http" ? (
-            <div style={{paddingBottom: 0}}>
-              <Link href={post.content} target="_blank" noWrap>
-                <Typography noWrap sx={{marginTop:1, marginBottom:0.5}}>
-                  {post.content}
-                </Typography>
-              </Link>
-              <CardContentNoPadding sx={{ padding: 0, paddingBottom:0 }}>
-                <div style={{ paddingBottom: 0 }}>
+            {post.contentType === "text/markdown" && (
+                <CardContent sx={{ padding: 0}}>
+                  <div style={{ display: "flex", justifyContent: "flex-start" }}>
+
+                  {/* https://www.npmjs.com/package/mui-markdown */}
+                  <MuiMarkdown>{`${post.content}`}</MuiMarkdown>
+                </div>
+                </CardContent>
+            )}
+            {post.contentType.includes("base64") && (
+              <CardContentNoPadding sx={{ padding: 0}}>
+                <div style={{ display: "flex", justifyContent: "center" }}>
                   <CardMedia
                     component="img"
                     style={{
@@ -97,85 +160,64 @@ const PostsList = ({
                   />
                 </div>
               </CardContentNoPadding>
-            </div>
-          ):(
-            post.contentType === "text/plain" && (
-              <Typography variant="body1">{post.content}</Typography>)
-          )}
-          {post.contentType === "text/markdown" && (
-              <CardContent sx={{ padding: 0}}>
-                <div style={{ display: "flex", justifyContent: "flex-start" }}>
-
-                 {/* https://www.npmjs.com/package/mui-markdown */}
-                <MuiMarkdown>{`${post.content}`}</MuiMarkdown>
-              </div>
-              </CardContent>
-          )}
-          {post.contentType.includes("base64") && (
-            <CardContentNoPadding sx={{ padding: 0}}>
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <CardMedia
-                  component="img"
-                  style={{
-                    maxWidth: "100%",
-                    width: "auto",
-                    borderRadius: 12,
-                  }}
-                  image={post.content}
-                />
-              </div>
-            </CardContentNoPadding>
-          )}
-          </CardContent>
-          <CardContent sx={{paddingLeft: 8.5, paddingTop: 1, paddingBottom: 0}}>
-            <PostCategories categories={post.categories}/>
-          </CardContent>
-          <CardContent sx={{paddingTop: 0.5, paddingBottom: 0, paddingLeft: 7.5}}>
-            <Grid container
-              justifyContent="space-between"
-              paddingLeft={0.5}
-              sx={{
-                width: "100%"
-              }}
-            >
-              <Grid item xs={4}>
-                <PostLikes post={post} />
-              </Grid>
-              <Grid item xs={4} container justifyContent="center">
-                <Tooltip title="Comment" placement="bottom-end">
-                <Button
-                  size="small"
-                  sx={{
-                    borderRadius: 100,
-                    minWidth: 0,
-                    color: "text.secondary"
-                  }}
-                  onClick={
-                    () => {
+            )}
+            </CardContent>
+            <CardContent sx={{paddingLeft: 8.5, paddingTop: 1, paddingBottom: 0}}>
+              <PostCategories categories={post.categories}/>
+            </CardContent>
+            <CardContent sx={{paddingTop: 0.5, paddingBottom: 0, paddingLeft: 7.5}}>
+              <Grid container
+                justifyContent="space-between"
+                paddingLeft={0.5}
+                sx={{
+                  width: "100%"
+                }}
+              >
+                <Grid item xs={4}>
+                  <PostLikes post={post} />
+                </Grid>
+                <Grid item xs={4} container justifyContent="center">
+                  <Tooltip title="Comment" placement="bottom-end">
+                  <Button
+                    size="small"
+                    sx={{
+                      borderRadius: 100,
+                      minWidth: 0,
+                      color: "text.secondary"
+                    }}
+                    onMouseDown={event => event.stopPropagation()}
+                    onClick={event => {
+                      event.stopPropagation();
+                      event.preventDefault();
                       openMakeCommentModal(post);
-                    }
-                  }
-                >
-                  <ChatBubbleOutlineIcon fontSize="small" />
-                  <Typography sx={{marginLeft: 1}}>
-                    {post.count}
-                  </Typography>
-                </Button>
-                </Tooltip>
+                    }}
+                  >
+                    <ChatBubbleOutlineIcon fontSize="small" />
+                    <Typography sx={{marginLeft: 1}}>
+                      {post.count}
+                    </Typography>
+                  </Button>
+                  </Tooltip>
+                </Grid>
+                <Grid item xs={4} container justifyContent="flex-end">
+                  <Tooltip title="Share" placement="bottom-end">
+                  <IconButton
+                    size="small"
+                    sx={{ marginRight: 1 }}
+                    onMouseDown={event => event.stopPropagation()}
+                    onClick={event => {
+                      event.stopPropagation();
+                      event.preventDefault();
+                      handleShare();
+                    }}
+                  >
+                    <ShareIcon fontSize="medium" />
+                  </IconButton>
+                  </Tooltip>
+                </Grid>
               </Grid>
-              <Grid item xs={4} container justifyContent="flex-end">
-                <Tooltip title="Share" placement="bottom-end">
-                <IconButton
-                  size="small"
-                  sx={{ marginRight: 1 }}
-                  onClick={handleShare}
-                >
-                  <ShareIcon fontSize="medium" />
-                </IconButton>
-                </Tooltip>
-              </Grid>
-            </Grid>
-          </CardContent>
+            </CardContent>
+          </CardActionArea>
         </Card>
       )))
       : (
