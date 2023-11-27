c8b92d736aa65e5b84ce1edd897c8017f660e672
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-11-26 23:33:41 -0700

    add localhost w/o /api

--- client/src/lists/lists.ts
+++ client/src/lists/lists.ts
@@ -3,6 +3,7 @@ import { Hosts } from "../enums/enums";
 export const localAuthorHosts = [
   process.env.REACT_APP_URI,
   Hosts.NOTFOUND,
+  "http://localhost:8000/",
 ];
 
 export const remoteAuthorHosts = [
