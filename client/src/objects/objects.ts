163cfd247a162b3071a7c8a0ef90e8d0270f3f8e
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-11-28 22:32:16 -0700

    REMOVE AFTER: localhost

--- client/src/objects/objects.ts
+++ client/src/objects/objects.ts
@@ -1,7 +1,7 @@
 export const codes: { [key: string]: string } = {
   "https://chimp-chat-1e0cca1cc8ce.herokuapp.com/": "chimpchatapi",
-  "localhost:8000/": "test",
-  "localhost:8000/api/": "test",
+  "http://localhost:8000/": "test",
+  "http://localhost:8000/api/": "test",
 };
 
 export const authorsListSubheader: { [key: string]: string } = {
