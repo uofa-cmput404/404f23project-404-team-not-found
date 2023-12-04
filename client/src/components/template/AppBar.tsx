28532e3732439691bc9ec97dfb91b1755c999c39
rmgutierrez <raphael_0810@outlook.com>
2023-12-04 00:28:47 -0700

    Resolution test

--- client/src/components/template/AppBar.tsx
+++ client/src/components/template/AppBar.tsx
@@ -54,7 +54,7 @@ const HeadBar = () => {
           marginLeft: "calc(15vw - 16px)",
         }}
       >
-            <Image src="https://i.postimg.cc/NFTYysSP/logof.png" width={160}/>
+            <Image src="https://i.postimg.cc/wjZxRjXw/Frame-1.png" width={160}/>
       </Link>
 
       <Button
